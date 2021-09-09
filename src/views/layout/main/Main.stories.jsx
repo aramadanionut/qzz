78772d35cb74ea298d206e9438370c70ca3ea09c
Dan Arama <danarama@coherentsolutions.com>
2021-09-09 21:45:10 +0300

    Add normalize.css, fonts and global styles

---
+++ src/views/layout/main/Main.stories.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+import Main from './Main';
+
+export default {
+  component: Main,
+  title: 'Components/Main',
+}
+
+export const Primary = () => <Main>This is the main layout component</Main>;
