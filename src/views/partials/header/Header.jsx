1daa594d9b850ce96bdb88b266b63223f98f8ad4
Dan Arama <danarama@coherentsolutions.com>
2021-09-09 23:40:11 +0300

    Update main layout and header styling

--- src/views/partials/header/Header.jsx
+++ src/views/partials/header/Header.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 
 import Logo from 'components/logo/Logo';
+import Avatar from 'components/avatar/Avatar';
 import { SIZES } from 'utils/constants';
 
 import './Header.scss';
@@ -8,7 +9,12 @@ import './Header.scss';
 export default function Header(props) {
     return (
         <header className="Header">
-            <Logo size={ SIZES.SMALL }></Logo>
+            <div className="Header__logo">
+                <Logo size={ SIZES.SMALL }></Logo>
+            </div>
+            <div className="Header__avatar">
+                <Avatar></Avatar>
+            </div>
         </header>
     );
 }
