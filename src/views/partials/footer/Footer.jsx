36929b1c1d37b8be0e9aa3ddcfbe2e99ec66e7d8
Dan Arama <danarama@coherentsolutions.com>
2021-09-12 20:11:44 +0300

    Add Footer links

--- src/views/partials/footer/Footer.jsx
+++ src/views/partials/footer/Footer.jsx
@@ -14,9 +14,16 @@ export default function Footer(props) {
         <div className="Footer">
             <div className="Footer__copyright">{`Copyright ${copyrightYears}`}</div>
             <div className="Footer__separator">/</div>
-            <div className="Footer__built-by">Built by <span className="Footer__author">danarama</span> </div>
+            <div className="Footer__built-by">
+                Built by <a target="_blank" href="https://github.com/aramadanionut" className="Footer__author">danarama</a> 
+            </div>
             <div className="Footer__separator">/</div>
-            <div className="Footer__repo"><FontAwesomeIcon icon={ faGithubSquare }/></div>
+            <a
+                target="_blank"
+                href="https://github.com/aramadanionut/qzz"
+                className="Footer__repo">
+                <FontAwesomeIcon icon={ faGithubSquare }/>
+            </a>
         </div>
     )
 }
