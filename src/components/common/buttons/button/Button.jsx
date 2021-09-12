8b31c61712174d4958f15935219f1470932ed64e
Dan Arama <danarama@coherentsolutions.com>
2021-09-12 14:30:50 +0300

    Update Timer styling

--- src/components/common/buttons/button/Button.jsx
+++ src/components/common/buttons/button/Button.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import PropTypes from 'prop-types';
 import classNames from 'classnames';
+
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faChevronLeft, faChevronRight } from '@fortawesome/free-solid-svg-icons';
 
