bb350ecf1826d87f2c0b2c33d7930eea302c52fe
Dan Arama <danarama@coherentsolutions.com>
2021-09-13 13:00:44 +0300

    Improve Home and Login page responsive layout

--- src/views/pages/login/Login.jsx
+++ src/views/pages/login/Login.jsx
@@ -1,33 +1,44 @@
 import React from "react";
 import { useForm } from "react-hook-form";
 import { useHistory } from "react-router-dom";
+import classNames from "classnames";
 
 import Button from "components/common/buttons/button/Button";
 import { TextField } from "components/common/forms/text-field/TextField";
 import { AvatarPicker } from "components/avatar-picker/AvatarPicker";
 
 import { BUTTON_TYPES, COLORS, POSITIONS } from "utils/constants";
-import { useStore } from 'hooks/useStore';
-
-import './Login.scss';
 import { LOGIN_FORM } from "utils/forms";
 import { STORE_KEYS } from "utils/store-keys";
 
+import { useStore } from 'hooks/useStore';
+import useWindowSize from "hooks/useWindowSize";
+
+import './Login.scss';
+
 export default function Login(props) {
     const history = useHistory();
+    const windowSize = useWindowSize();
 
     const { register, watch, handleSubmit, formState } = useForm({ mode: 'onChange' });
-    const [ setUser ] = useStore(STORE_KEYS.USER);
+    
+    // eslint-disable-next-line no-unused-vars
+    const [ user, setUser ] = useStore(STORE_KEYS.USER);
 
     const username = watch(LOGIN_FORM.USERNAME);
     
     const onSubmit = ({ username, avatar }) => {
         setUser({ username, avatar });
         history.push('/builder');
-    }
+    };
+
+    const loginClassNames = classNames({
+        Login: true,
+        'Login--mobile': !!windowSize.isMobile
+    });
 
     return (
-        <div className="Login">
+        <div className={ loginClassNames }>
             <h3 className="Login__heading">Nice to meet you{ username && `, ${username}` }!</h3>
             <p className="Login__text">Before we start, tell us a bit about yourself</p>
 
