1061cb068ded8aeb8fc6dd39a9a86687fb12e344
Dan Arama <danarama@coherentsolutions.com>
2021-09-13 12:43:47 +0300

    Make Home page responsive

--- src/views/pages/home/Home.jsx
+++ src/views/pages/home/Home.jsx
@@ -2,15 +2,23 @@ import React from "react";
 import { Link } from 'react-router-dom';
 
 import Button from "components/common/buttons/button/Button";
+import { useStore } from "hooks/useStore";
+import useWindowSize from "hooks/useWindowSize";
 
 import './Home.scss';
-import { useStore } from "hooks/useStore";
-
+import classNames from "classnames";
 export default function Home(props) {
+    const windowSize = useWindowSize();
+
     const [ user ] = useStore('user');
 
+    const homeClasses = classNames({
+        Home: true,
+        'Home--mobile': !!windowSize.isMobile
+    });
+
     return (
-        <div className="Home">
+        <div className={ homeClasses }>
             <h3 className="Home__heading">Welcome!</h3>
             <p className="Home__text">Ready to put your knowledge to the test?</p>
 
